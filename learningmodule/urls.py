d2b96fa78e8989ee650d8ccdcae504f451b9c30f
rupin <rupin.chheda@gmail.com>
2019-03-31 19:52:43 +0530

    Fix

--- learningmodule/urls.py
+++ learningmodule/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from .views import *
+from . import views
 from django.conf.urls import url
 
 from django.contrib.auth import views as auth_views
