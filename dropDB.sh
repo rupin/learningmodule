fc1ac39a82e35b557ffa56ed2fb04206f8c4f365
rupin <rupin.chheda@gmail.com>
2019-03-31 17:03:51 +0530

    Fix

--- dropDB.sh
+++ dropDB.sh
@@ -1,23 +1,13 @@
 dropDB_cmd="python manage.py migrate learningmodule zero"
 eval $dropDB_cmd
-
 changeDir="cd learningmodule"
-
 eval $changeDir
-
 remove_mig_files="rm -r migrations"
-
 eval $remove_mig_files
-
 changeDir="cd .."
-
 eval $changeDir
-
 create_MIG_files="python manage.py makemigrations learningmodule"
-
 eval $create_MIG_files
-
 createDB="python manage.py migrate"
-
 eval $createDB
 
