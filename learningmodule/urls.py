00b40e428d09802bde02cff09d3d56ab95af3196
rupin <rupin.chheda@gmail.com>
2019-03-31 19:52:09 +0530

    Fix

--- learningmodule/urls.py
+++ learningmodule/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.conf.urls import url
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', views.homePage),
+	 url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ]
