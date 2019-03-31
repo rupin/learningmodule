9481c5d947546e54b8cd3566694a8047bde8f30a
rupin <rupin.chheda@gmail.com>
2019-03-31 15:11:14 +0530

    Fix

---
+++ learningmodule/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    pass
+
+#this model saves the modules currently being offered	
+class Module(models.Model):
+	module_id = models.BigIntegerField(primary_key=True)
+	module_name = models.CharField(max_length=50)
+    module_category = models.CharField(max_length=50)
+    module_grade= models.CharField(max_length=100)
+	
+
+#this model saves the relationship between which module has been offered by which user. It will also have schedule information associated with it.
+class Schedule(models.Model):
+	schedule_id=models.BigIntegerField(primary_key=True)
+	fk_module_id=models.ForeignKey(Module, on_delete=models.CASCADE)
+	fk_user_id = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+#this model saves the relationship between which module has been taken by which user, and which schedule it was done on.
+class Milestones(model.Models):	
+	milestone_id=models.BigIntegerField(primary_key=True)
+	fk_schedule_id=models.ForeignKey(Schedule, on_delete=models.CASCADE)
+	fk_module_id=models.ForeignKey(Module, on_delete=models.CASCADE)
+	fk_user_id = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
--- learningmodule/settings.py
+++ learningmodule/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ESPWeigh.wsgi.application'
-
+AUTH_USER_MODEL = 'models.User'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
